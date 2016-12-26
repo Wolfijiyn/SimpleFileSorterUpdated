0102eea096cf04ae0dcabc41c5709c8e121dbb69
Wolfijiyn <Jonnykhanas21@gmail.com>
2016-12-26 19:09:10 +0100

    Add files via upload

---
+++ dropshit/Core/FileSorter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace dropshit.Core
+{
+    class FileSorter
+    {
+        public FileSorter() { }
+
+        public int Sort(string[] files)
+        {
+            int notCounted = 0;
+
+            foreach (string filePath in files)
+            {
+                if (Path.GetExtension(filePath) != "")
+                {
+                    string folderName = Path.GetExtension(filePath).Replace(".", "").ToUpper() + "S";
+                    notCounted = notCounted + Sort(Path.GetExtension(filePath), folderName, filePath);
+                }
+            }
+
+            return notCounted;
+        }
+
+        public int Sort(string ext, string folderName, string fileName)
+        {
+            try
+            {
+                string _fileName = Path.GetFileName(fileName);
+
+                string _folderPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                string combinedPaths = System.IO.Path.Combine(_folderPath, folderName);
+
+                string targetPath = System.IO.Path.Combine(combinedPaths, _fileName);
+
+                if (!Directory.Exists(combinedPaths))
+                    System.IO.Directory.CreateDirectory(combinedPaths);
+
+                if (File.Exists(targetPath))
+                    return 1;
+
+                if (!File.Exists(targetPath))
+                {
+                    System.IO.File.Move(fileName, targetPath);
+                    return 0;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ dropshit/frmMain.cs
@@ -0,0 +1,73 @@
+﻿using dropshit.Core;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace dropshit
+{
+    public partial class frmMain : Form
+    {
+        private FileSorter fSorter = new FileSorter();
+
+        public frmMain()
+        {
+            AllowDrop = true;
+            InitializeComponent();
+
+           
+        }
+
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+        
+        private void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            lBox.Items.AddRange(files);
+
+            int notCounted = fSorter.Sort(files);
+
+            if (notCounted > 0 || notCounted >= files.Length)
+            {
+                MessageBox.Show(notCounted.ToString() + " Files were not sorted due to the fact of them having the same name as some of the ones that has already been sorted.");
+                lBox.Items.Clear();
+                return;
+            }
+
+            MessageBox.Show("Sorted (" + files.Length.ToString() + ") files");
+
+            lBox.Items.Clear();
+        }
+
+        private void frmMain_Move(object sender, EventArgs e)
+        {
+            //When minimized we create a BallonTip at the bottom right of our screen
+            //with a title and a message
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon1.ShowBalloonTip(5000, "Title", "Your Message", ToolTipIcon.Info);
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            //When the icon is double clicked we bring the form back up.
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            //When the balloonTip is clicked then we bring the form back up.
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
+
+
